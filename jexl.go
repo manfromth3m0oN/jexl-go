5aa4e2b913fa35eef5fc8cee67df940ef94c6c3d
Max S-T <maxstantay@gmail.com>
2023-01-19 15:17:58 +0000

    document engine

--- jexl.go
+++ jexl.go
@@ -21,10 +21,14 @@ func Eval(expression string) (string, error) {
 	return C.GoString(resCString), nil
 }
 
+// Engine holds a reference to the Rust jexl engine object to allow for long running FFI
 type Engine struct {
 	engine *C.JexlEngine
 }
 
+// NewEngine creates a new engine with a context and script.
+// This means you should use one engine per script.
+// Don't forget to call free on your engine, you'll leak memory otherwise.
 func NewEngine(context interface{}, script string) (Engine, error) {
 	b, err := json.Marshal(context)
 	if err != nil {
@@ -36,11 +40,13 @@ func NewEngine(context interface{}, script string) (Engine, error) {
 	}, nil
 }
 
+// Run will run the script provided at Initialization in the context also provided.
 func (e Engine) Run() string {
 	c_res := C.run_engine(e.engine)
 	return C.GoString(c_res)
 }
 
+// Free instructs the Rust code to drop the memory associated with this frontend
 func (e Engine) Free() {
 	C.free_engine(e.engine)
 }
