a0d41d30e3156fec8ce99a68825a1a280e89dd25
Max S-T <maxstantay@gmail.com>
2023-01-19 13:35:12 +0000

    add docs

--- jexl.go
+++ jexl.go
@@ -1,3 +1,7 @@
+/*
+jexl is a library to evaluate the jexl scripting language in Go.
+This implementation relies on the Rust library `jexl-rs`.
+*/
 package jexl
 
 /*
@@ -7,6 +11,9 @@ package jexl
 */
 import "C"
 
+// Eval evaluates an expression, whether that be an entire script or something smaller.
+// This function relies on FFI to function. The expression is parsed into a C String then handed off to Rust.
+// The Rust code calls to the jexl-rs library to actually evaluate the expression then hand it back to Go.
 func Eval(expression string) (string, error) {
 	cExpr := C.CString(expression)
 	resCString := C.eval(cExpr)
